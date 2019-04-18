e6ebc11d78f41a6139ccfc2209b4e0bf2bb5df58
Sh-x <39991480+Sh-x@users.noreply.github.com>
2019-04-18 20:20:54 +0800

    version2.0
    now you can choose the difficulty

--- MSCore.java
+++ MSCore.java
@@ -7,23 +7,47 @@ import java.util.*;
 * @version 1.0
 */
 public class MSCore {
-	private int gridLength = 9; // edge length
-	private int[][] grid = new int[gridLength][gridLength]; // a matrix to store the values of each box in the chessboard
-	                                                        // -1 means there is a mine.
-	private int[][] memory = new int[gridLength][gridLength]; // a matrix to store whether the box has been checked.
-															  // 0 means has not been checked.
-															  // 1 means user chose this box before.
-	                            	// the value bigger than 1 means user did not choose this box but this box has been checked.
-	private int mineNum = 10; // the number of mines.
+	private int gridLength; // edge length
+	private int[][] grid; 	// a matrix to store the values of each box in the chessboard
+							// -1 means there is a mine.
+	private int[][] memory; // a matrix to store whether the box has been checked.
+							// 0 means has not been checked.
+							// 1 means user chose this box before.
+							// the value bigger than 1 means user did not choose this box but this box has been checked.
+	private int mineNum; // the number of mines.
 
+	/**
+	 * Constructor of MSCore
+	 * @param gridLength length of game board
+	 * @param mineNum number of mines
+	 */
+	public MSCore(int gridLength, int mineNum) {
+		this.gridLength = gridLength;
+		this.mineNum = mineNum;
+		this.grid = new int[gridLength][gridLength];
+		this.memory = new int[gridLength][gridLength];
+	}
+
+	/**
+	 * Getter of gridLenght
+	 * @return gridlength
+	 */
 	public int getGridLength() {
 		return this.gridLength;
 	}
 
+	/**
+	 * Getter of grid
+	 * @return grid
+	 */
 	public int[][] getGrid() {
 		return this.grid;
 	}
 
+	/**
+	 * Getter of memory
+	 * @return memory
+	 */
 	public int[][] getMemory() {
 		return this.memory;
 	}
@@ -32,30 +56,30 @@ public class MSCore {
 	* and calculate the values of each box.
 	*/
 	public void startGame() {
-		for(int k = 0; k < mineNum; k++) {
-			int i = (int)(Math.random() * gridLength);
-			int j = (int)(Math.random() * gridLength);
-			if(grid[i][j] == -1) {
-				k--;
-				continue;
-			}
-			grid[i][j] = -1;
-			if(j < gridLength - 1 && grid[i][j + 1] != -1)
-				grid[i][j + 1]++;
-			if(j > 0 && grid[i][j - 1] != -1)
-				grid[i][j - 1]++;
-			if(i < gridLength - 1 && grid[i + 1][j] != -1)
-				grid[i + 1][j]++;
-			if(i > 0 && grid[i - 1][j] != -1)
-				grid[i - 1][j]++;
-			if(i > 0 && j > 0 && grid[i - 1][j - 1] != -1)
-				grid[i - 1][j - 1]++;
-			if(i > 0 && j < gridLength - 1 && grid[i - 1][j + 1] != -1)
-				grid[i - 1][j + 1]++;
-			if(i < gridLength - 1 && j > 0 && grid[i + 1][j - 1] != -1)
-				grid[i + 1][j - 1]++;
-			if(i < gridLength - 1 && j < gridLength - 1 && grid[i + 1][j + 1] != -1)
-				grid[i + 1][j + 1]++;
+	for(int k = 0; k < mineNum; k++) {
+		int i = (int)(Math.random() * gridLength);
+		int j = (int)(Math.random() * gridLength);
+		if(grid[i][j] == -1) { //This block has already been a mine.
+			k--;
+			continue;
+		}
+		grid[i][j] = -1;
+		if(j < gridLength - 1 && grid[i][j + 1] != -1)
+			grid[i][j + 1]++;
+		if(j > 0 && grid[i][j - 1] != -1)
+			grid[i][j - 1]++;
+		if(i < gridLength - 1 && grid[i + 1][j] != -1)
+			grid[i + 1][j]++;
+		if(i > 0 && grid[i - 1][j] != -1)
+			grid[i - 1][j]++;
+		if(i > 0 && j > 0 && grid[i - 1][j - 1] != -1)
+			grid[i - 1][j - 1]++;
+		if(i > 0 && j < gridLength - 1 && grid[i - 1][j + 1] != -1)
+			grid[i - 1][j + 1]++;
+		if(i < gridLength - 1 && j > 0 && grid[i + 1][j - 1] != -1)
+			grid[i + 1][j - 1]++;
+		if(i < gridLength - 1 && j < gridLength - 1 && grid[i + 1][j + 1] != -1)
+			grid[i + 1][j + 1]++;
 		}
 	}
 
